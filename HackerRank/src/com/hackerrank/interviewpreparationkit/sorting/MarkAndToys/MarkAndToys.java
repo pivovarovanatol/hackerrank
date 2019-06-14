2a59c222acecf0a5b5317cc97e61a7f591961c5d
I855719 <I855719@VANN00632494A.amer.global.corp.sap>
2019-06-14 09:58:19 -0700

    com.hackerrank.interviewpreparationkit.sorting.MarkAndToys - Solved, Submitted, Accepted.

--- HackerRank/src/com/hackerrank/interviewpreparationkit/sorting/MarkAndToys/MarkAndToys.java
+++ HackerRank/src/com/hackerrank/interviewpreparationkit/sorting/MarkAndToys/MarkAndToys.java
@@ -8,24 +8,81 @@ import java.util.Scanner;
 
 public class MarkAndToys {
 
+	
+	
+	
     // Complete the maximumToys function below.
     static int maximumToys(int[] prices, int k) {
-    	
-    	System.out.println("Initial array: ");
+    	sort(prices);
+    	//printArray(prices);
+    	//sort2(prices);
     	for (int i=0;i<prices.length;i++) {
-    		System.out.print(Integer.toString(prices[i]) + " ");
+    		k -= prices[i];
+    		
+    		if (k < 0) {
+    			return i;
+    		}
     	}
-    	System.out.println();
-    	System.out.println("Money: " + Integer.toString(k)); 
-    	
-    	
-    	
-    	
-    	return 0;
+    	return prices.length;
     }
 
     //private static final Scanner scanner = new Scanner(System.in);
 
+    public static void makeHeap2(int[] arr, int i, int n) {
+    	int smallest = i;
+    	int left = i*2 +1;
+    	int right = i*2 +2;
+    	
+    	if (left < n && arr[left] < arr[smallest]) {
+        	smallest = left;
+    	}
+    	
+    	if (right < n && arr[right] < arr[smallest]) {
+        	smallest = right;
+    	}
+    	
+    	if (smallest != i) {
+    		int swap = arr[i];
+    		arr[i] = arr[smallest];
+    		arr[smallest] = swap;
+    		
+    		makeHeap2(arr, smallest, n);
+    	}
+    	
+    }
+    
+    
+    
+    public static void sort2(int[] arr) {
+    	
+    	int n = arr.length;
+    	
+    	for (int i=n; i>=0;i--) {
+        	makeHeap2(arr, i, n);
+    	}
+
+    	printArray(arr);
+
+        // One by one extract an element from heap 
+        for (int i=n-1; i>=0; i--) 
+        { 
+            // Move current root to end 
+            int temp = arr[0]; 
+            arr[0] = arr[i]; 
+            arr[i] = temp; 
+
+            printArray(arr);
+  
+            // call max heapify on the reduced heap 
+            makeHeap2(arr, 0, i); 
+            printArray(arr);
+        }     	
+    	
+    	printArray(arr);
+        
+        
+    }
+    
     
     public static void main(String[] args) throws IOException {
 
@@ -53,8 +110,10 @@ public class MarkAndToys {
             prices[i] = pricesItem;
         }
 
+        //printArray(prices);
         int result = maximumToys(prices, k);
 
+        //System.out.println(result);
         bufferedWriter.write(String.valueOf(result));
         bufferedWriter.newLine();
 
@@ -62,4 +121,67 @@ public class MarkAndToys {
 
         scanner.close();
     }
+
+
+    public static void sort(int arr[]) 
+    { 
+        int n = arr.length; 
+  
+        // Build heap (rearrange array) 
+        for (int i = n / 2 - 1; i >= 0; i--) 
+            heapify(arr, n, i); 
+  
+        // One by one extract an element from heap 
+        for (int i=n-1; i>=0; i--) 
+        { 
+            // Move current root to end 
+            int temp = arr[0]; 
+            arr[0] = arr[i]; 
+            arr[i] = temp; 
+  
+            // call max heapify on the reduced heap 
+            heapify(arr, i, 0); 
+        } 
+    } 
+
+
+    // To heapify a subtree rooted with node i which is 
+    // an index in arr[]. n is size of heap 
+    static void heapify(int arr[], int n, int i) 
+    { 
+        int largest = i; // Initialize largest as root 
+        int l = 2*i + 1; // left = 2*i + 1 
+        int r = 2*i + 2; // right = 2*i + 2 
+  
+        // If left child is larger than root 
+        if (l < n && arr[l] > arr[largest]) 
+            largest = l; 
+  
+        // If right child is larger than largest so far 
+        if (r < n && arr[r] > arr[largest]) 
+            largest = r; 
+  
+        // If largest is not root 
+        if (largest != i) 
+        { 
+            int swap = arr[i]; 
+            arr[i] = arr[largest]; 
+            arr[largest] = swap; 
+            // Recursively heapify the affected sub-tree 
+            heapify(arr, n, largest); 
+        } 
+    } 
+
+    /* A utility function to print array of size n */
+    static void printArray(int arr[]) 
+    { 
+        int n = arr.length; 
+        for (int i=0; i<n; ++i) 
+            System.out.print(arr[i]+" "); 
+        System.out.println(); 
+    } 
+
+
+
+
 }
