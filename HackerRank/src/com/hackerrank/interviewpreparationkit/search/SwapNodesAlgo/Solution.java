7ae27881f4b011cc48647f73e5fbc5d689abb879
I855719 <I855719@VANN00632494A.amer.global.corp.sap>
2019-07-16 08:40:55 -0700

    com.hackerrank.interviewpreparationkit.search.SwapNodesAlgo - initial changes

---
+++ HackerRank/src/com/hackerrank/interviewpreparationkit/search/SwapNodesAlgo/Solution.java
@@ -0,0 +1,93 @@
+package com.hackerrank.interviewpreparationkit.search.SwapNodesAlgo;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) throws IOException {
+
+    	Instant programStart = Instant.now();
+    	
+        File file = new File("C:\\Users\\i855719\\git\\hackerrank\\HackerRank\\src\\com\\hackerrank\\interviewpreparationkit\\search\\HashTablesIceCreamParlor\\data.txt");
+        Scanner scanner = new Scanner(file);
+        
+        File file_result = new File("C:\\Users\\i855719\\git\\hackerrank\\HackerRank\\src\\com\\hackerrank\\interviewpreparationkit\\search\\HashTablesIceCreamParlor\\result.txt");
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file_result));
+
+        int n = Integer.parseInt(scanner.nextLine().trim());
+
+        int[][] indexes = new int[n][2];
+
+        for (int indexesRowItr = 0; indexesRowItr < n; indexesRowItr++) {
+            String[] indexesRowItems = scanner.nextLine().split(" ");
+
+            for (int indexesColumnItr = 0; indexesColumnItr < 2; indexesColumnItr++) {
+                int indexesItem = Integer.parseInt(indexesRowItems[indexesColumnItr].trim());
+                indexes[indexesRowItr][indexesColumnItr] = indexesItem;
+            }
+        }
+
+        int queriesCount = Integer.parseInt(scanner.nextLine().trim());
+
+        int[] queries = new int[queriesCount];
+
+        for (int queriesItr = 0; queriesItr < queriesCount; queriesItr++) {
+            int queriesItem = Integer.parseInt(scanner.nextLine().trim());
+            queries[queriesItr] = queriesItem;
+        }
+
+        int[][] result = swapNodes(indexes, queries);
+
+        for (int resultRowItr = 0; resultRowItr < result.length; resultRowItr++) {
+            for (int resultColumnItr = 0; resultColumnItr < result[resultRowItr].length; resultColumnItr++) {
+                bufferedWriter.write(String.valueOf(result[resultRowItr][resultColumnItr]));
+
+                if (resultColumnItr != result[resultRowItr].length - 1) {
+                    bufferedWriter.write(" ");
+                }
+            }
+
+            if (resultRowItr != result.length - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+        
+        bufferedWriter.close();
+        scanner.close();
+
+        Instant programEnd = Instant.now();
+        Duration timeElapsed = Duration.between(programStart, programEnd);
+        System.out.println("Time taken: "+ timeElapsed.toMillis() +" milliseconds");
+
+        
+    }
+    
+    
+    /*
+     * Complete the swapNodes function below.
+     */
+    static int[][] swapNodes(int[][] indexes, int[] queries) {
+
+    }
+    
+    
+    
+    
+}
+
+
