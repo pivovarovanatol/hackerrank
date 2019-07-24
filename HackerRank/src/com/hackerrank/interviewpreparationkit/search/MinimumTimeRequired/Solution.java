1ebcfd498a8212f75bcf317ac9dd4058c3911c2a
I855719 <I855719@VANN00632494A.amer.global.corp.sap>
2019-07-24 16:41:25 -0700

    com.hackerrank.interviewpreparationkit.search.MinimumTimeRequired - Initial changes

---
+++ HackerRank/src/com/hackerrank/interviewpreparationkit/search/MinimumTimeRequired/Solution.java
@@ -0,0 +1,82 @@
+package com.hackerrank.interviewpreparationkit.search.MinimumTimeRequired;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) throws IOException {
+
+    	Instant programStart = Instant.now();
+    	
+        File file = new File("C:\\Users\\i855719\\git\\hackerrank\\HackerRank\\src\\com\\hackerrank\\interviewpreparationkit\\search\\MinimumTimeRequired\\data1.txt");
+        Scanner scanner = new Scanner(file);
+        
+        File file_result = new File("C:\\Users\\i855719\\git\\hackerrank\\HackerRank\\src\\com\\hackerrank\\interviewpreparationkit\\search\\MinimumTimeRequired\\result.txt");
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file_result));
+
+        String[] nGoal = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nGoal[0]);
+
+        long goal = Long.parseLong(nGoal[1]);
+
+        long[] machines = new long[n];
+
+        String[] machinesItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            long machinesItem = Long.parseLong(machinesItems[i]);
+            machines[i] = machinesItem;
+        }
+
+        long ans = minTime(machines, goal);
+
+        bufferedWriter.write(String.valueOf(ans));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+
+        System.out.println(ans);
+        Instant programEnd = Instant.now();
+        Duration timeElapsed = Duration.between(programStart, programEnd);
+        System.out.println("Time taken: "+ timeElapsed.toMillis() +" milliseconds");
+
+        
+    }
+    
+    
+
+    // Complete the minTime function below.
+    static long minTime(long[] machines, long goal) {
+    	
+
+    	return goal;
+    }
+
+    static void printArray(int[] arr) {
+    	for (int i=0;i<arr.length;i++) {
+    		System.out.print(arr[i] + " ");
+    	}
+    	System.out.println();
+    }
+}
+
+
