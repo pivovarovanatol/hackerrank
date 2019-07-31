382d5f57456274a6955700d9a7e010b0d85da6de
I855719 <I855719@VANN00632494A.amer.global.corp.sap>
2019-07-31 09:08:44 -0700

    url added

--- HackerRank/src/com/hackerrank/interviewpreparationkit/search/HashTablesIceCreamParlor/Solution.java
+++ HackerRank/src/com/hackerrank/interviewpreparationkit/search/HashTablesIceCreamParlor/Solution.java
@@ -1,4 +1,5 @@
 package com.hackerrank.interviewpreparationkit.search.HashTablesIceCreamParlor;
+// https://www.hackerrank.com/challenges/ctci-ice-cream-parlor/problem
 
 import java.io.BufferedWriter;
 import java.io.File;
