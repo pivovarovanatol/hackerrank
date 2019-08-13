5464c9604d8b22aa0154b03a6d89ff18ef1d7e06
I855719 <I855719@VANN00632494A.amer.global.corp.sap>
2019-08-13 15:30:32 -0700

    com.hackerrank.interviewpreparationkit.StringManipulation.SherlockAndValidString - initial changes

---
+++ HackerRank/src/com/hackerrank/interviewpreparationkit/StringManipulation/SherlockAndValidString/Solution.java
@@ -0,0 +1,62 @@
+package com.hackerrank.interviewpreparationkit.StringManipulation.SherlockAndValidString;
+// https://www.hackerrank.com/challenges/sherlock-and-valid-string/
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) throws IOException {
+
+    	Instant programStart = Instant.now();
+    	
+        File file = new File("C:\\Users\\i855719\\git\\hackerrank\\HackerRank\\src\\com\\hackerrank\\interviewpreparationkit\\StringManipulation\\MakingAnagrams\\data.txt");
+        Scanner scanner = new Scanner(file);
+        
+        File file_result = new File("C:\\Users\\i855719\\git\\hackerrank\\HackerRank\\src\\com\\hackerrank\\interviewpreparationkit\\StringManipulation\\MakingAnagrams\\result.txt");
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file_result));
+
+        String s = scanner.nextLine();
+
+        String result = isValid(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+
+        Instant programEnd = Instant.now();
+        Duration timeElapsed = Duration.between(programStart, programEnd);
+        System.out.println("Time taken: "+ timeElapsed.toMillis() +" milliseconds");
+
+        
+    }
+    
+    
+    // Complete the isValid function below.
+    static String isValid(String s) {
+
+
+    	return s;
+    }
+    
+}
+
+
