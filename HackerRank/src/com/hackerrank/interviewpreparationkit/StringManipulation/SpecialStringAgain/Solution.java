6742cc54658cd4b61a212a2153c156b2ce50ec21
I855719 <I855719@VANN00632494A.amer.global.corp.sap>
2019-08-22 09:10:46 -0700

    com.hackerrank.interviewpreparationkit.StringManipulation.SpecialStringAgain - solved, submitted, tested, accepted. Get the idea from the discussions section and adapted to Java. Need to explore more aboput combinations....

--- HackerRank/src/com/hackerrank/interviewpreparationkit/StringManipulation/SpecialStringAgain/Solution.java
+++ HackerRank/src/com/hackerrank/interviewpreparationkit/StringManipulation/SpecialStringAgain/Solution.java
@@ -16,6 +16,7 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Hashtable;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -55,9 +56,98 @@ public class Solution {
         
     }
     
+
+    static class Point {
+        public char key;
+        public long count;
+
+        public Point(char x, long y) {
+            key = x;
+            count = y;
+        }
+    }
+
+    
     
     // Complete the substrCount function below.
     static long substrCount(int n, String s) {
+        long result = 0;
+        char ch = '0';
+        char oldCh = '0';
+        long counter = 0;
+        long oldCounter = 0;
+        long leftCounter = 0;
+        boolean special = false;
+        for(char val : s.toCharArray()) {
+            if (val == ch) {
+            	++counter;
+            	}
+            else
+            {
+                if (special) {
+                    result += counter < leftCounter ? counter : leftCounter;
+                }
+                special = counter == 1 && val == oldCh;
+                if (special) {
+                    leftCounter = oldCounter;
+                }
+                result += counter * (counter + 1) / 2;
+                oldCh = ch;
+                ch = val;
+                oldCounter = counter;
+                counter = 1;
+            }
+        };
+        if (special)
+            result += counter < leftCounter ? counter : leftCounter;
+        result += counter * (counter + 1) / 2;
+        return result;
+    }
+    
+    
+    // Complete the substrCount function below.
+    static long substrCount3(int n, String s) {
+        s = s + " ";
+        ArrayList<Point> l = new ArrayList<Point>();
+        long count = 1;
+        char ch = s.charAt(0);
+        for(int i = 1; i <= n ; i++) {
+            if(ch == s.charAt(i))
+                count++;
+            else {
+                l.add(new Point(ch, count));
+                count = 1;
+                ch = s.charAt(i);
+            }
+        }
+        count = 0;
+        if(l.size() >= 3) {   
+            Iterator<Point> itr = l.iterator();
+            Point prev, curr, next;
+            curr = (Point)itr.next();
+            next = (Point)itr.next();
+            count = (curr.count * (curr.count + 1)) / 2;
+            for(int i = 1; i < l.size() - 1; i++) {
+                prev = curr;
+                curr = next;
+                next = itr.next();
+                count += (curr.count * (curr.count + 1)) / 2;
+                if(prev.key == next.key && curr.count == 1)
+                    count += prev.count > next.count ? next.count : prev.count;
+            }
+            count += (next.count * (next.count + 1)) / 2;
+        } else {
+            for(Point curr:l){
+                count += (curr.count * (curr.count + 1)) / 2;
+            }
+        }
+        return count;
+    }  
+    
+
+    
+    // Complete the substrCount function below.
+    static long substrCount2(int n, String s) {
 
     	char[] arr = s.toCharArray();
     	//System.out.println(isSpecial(arr,0,arr.length));
